32ad195f756cd7890afc26d747aa76c79f86f8a7
Conrad Khakhria <conradjkhakhria@gmail.com>
2021-07-23 19:12:19 +0100

    expressions have their 'if' exprs moved to the top level before CPS conversion

--- src/preproc/preproc.rs
+++ src/preproc/preproc.rs
@@ -3,11 +3,12 @@ use crate::preproc::{ cps, fmt };
 
 use std::rc::Rc;
 
-pub fn preprocess(value: Rc<Value>) -> Rc<Value> {
+pub fn preprocess(expr: Rc<Value>) -> Rc<Value> {
     /* Preprocesses a value */
 
-    let formatted = fmt::format_names(value);
-    let cps_converted = cps::convert(formatted, None, None);
+    let formatted = fmt::format_names(expr);
+    let if_rearranged = cps::move_ifs(formatted);
+    let cps_converted = cps::convert(if_rearranged, None);
 
     return cps_converted;
 }
