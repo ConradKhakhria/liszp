8613a9bbb9e8cbf42cba6960e77787655e3e8775
Conrad Khakhria <conradjkhakhria@gmail.com>
2021-08-23 12:22:30 +0100

    unswitched operator loops as well as removing unnecessary clones of rug types

--- src/eval/operators.rs
+++ src/eval/operators.rs
@@ -11,39 +11,41 @@ use rug;
 
 /* Arithmetic */
 
-fn float_arithmetic(op: String, numbers: LinkedList<Rc<Value>>) -> Value {
+fn float_arithmetic(op: String, numbers: LinkedList<&Rc<Value>>) -> Value {
     /* Evaluates an arithmetic expression where one or more of the parameters are floats */
 
-    let mut result = match &**numbers.front().unwrap() {
+    let mut result = match &***numbers.front().unwrap() {
         Value::Float(f) => f.clone(),
         Value::Integer(i) => rug::Float::with_val(53, i),
         _ => panic!("This really shouldn't happen")
     };
 
-    for n in numbers.iter().dropping(1) {
-        match &**n {
-            Value::Float(f) => {
-                match &op[..] {
-                    "+&" => result += f,
-                    "-&" => result -= f,
-                    "*&" => result *= f,
-                    "/&" => result /= f,
-                    _    => result %= f
+    macro_rules! reduce_stmt {
+        { $action:tt } => {
+            for n in numbers.iter().dropping(1) {
+                match &***n {
+                    Value::Float(f) => { result $action f },
+                    Value::Integer(i) => { result $action i },
+                    _ => panic!("This should never happen")
                 }
-            },
+            }
+        }
+    }
 
-            Value::Integer(i) => {
-                match &op[..] {
-                    "+&" => result += i,
-                    "-&" => result -= i,
-                    "*&" => result *= i,
-                    "/&" => result /= i,
-                    _    => panic!("Cannot take modulo of a float by an integer")
+    match &op[..] {
+        "+&" => reduce_stmt!(+=),
+        "-&" => reduce_stmt!(-=),
+        "*&" => reduce_stmt!(*=),
+        "/&" => reduce_stmt!(/=),
+         _   => {
+            for n in numbers.iter().dropping(1) {
+                match &***n {
+                    Value::Float(f) => result %= f,
+                    Value::Integer(i) => panic!("cannot take integer modulo of float"),
+                    _ => panic!("This should never happen")
                 }
-            },
-
-            _ => panic!("This shouldn't happen ever")
-        };
+            }
+         }
     }
 
     return if &op[..] == "-&" && numbers.len() == 1 {
@@ -53,30 +55,31 @@ fn float_arithmetic(op: String, numbers: LinkedList<Rc<Value>>) -> Value {
     };
 }
 
-fn integer_arithmetic(op: String, numbers: LinkedList<Rc<Value>>) -> Value {
+fn integer_arithmetic(op: String, numbers: LinkedList<&Rc<Value>>) -> Value {
     /* Evaluates an arithmetic expression consisting of all integers */
 
-    macro_rules! get_integer {
-        ( $x:expr ) => {
-            match &**$x {
-                Value::Integer(i) => i,
-                _ => panic!("This absolutely shouldn't happen")
+    let mut result = match &***numbers.front().unwrap() {
+        Value::Integer(i) => i.clone(),
+        _ => panic!("This absolutely shouldn't happen")
+    };
+
+    macro_rules! reduce_stmt {
+        { $action:tt } => {
+            for n in numbers.iter().dropping(1) {
+                match &***n {
+                    Value::Integer(i) => { result $action i },
+                    _ => panic!("This should never happen")
+                }
             }
-        };
+        }
     }
 
-    let mut result = get_integer!(numbers.front().unwrap()).clone();
-
-    for n in numbers.iter().dropping(1) {
-        let num = get_integer!(n);
-
-        match &op[..] {
-            "+&" => result += num,
-            "-&" => result -= num,
-            "*&" => result *= num,
-            "/&" => result /= num,
-            _    => result %= num
-        }
+    match &op[..] {
+        "+&" => reduce_stmt!(+=),
+        "-&" => reduce_stmt!(-=),
+        "*&" => reduce_stmt!(*=),
+        "/&" => reduce_stmt!(/=),
+        _    => reduce_stmt!(%=)
     }
 
     return if &op[..] == "-&" && numbers.len() == 1 {
@@ -108,15 +111,15 @@ pub (in crate::eval) fn arithmetic(op: String, parameters: Rc<Value>, env: &Env)
         match &**param {
             Value::Float(_) => {
                 floats = true;
-                numbers.push_back(Rc::clone(param));
+                numbers.push_back(param);
             },
 
             Value::Integer(_) => {
-                numbers.push_back(Rc::clone(param))
+                numbers.push_back(param)
             },
 
             Value::Name(_) => {
-                numbers.push_back(Rc::clone(&crate::eval::eval_main::resolve_value(param, env)))
+                numbers.push_back(crate::eval::eval_main::resolve_value(param, env))
             },
 
             _ => panic!("Expected number literal or variable containing number in '{}' expression", op)
@@ -134,7 +137,7 @@ pub (in crate::eval) fn arithmetic(op: String, parameters: Rc<Value>, env: &Env)
 
 /* Comparison */
 
-fn integer_comparison(op: String, x: rug::Integer, y: rug::Integer) -> Rc<Value> {
+fn integer_comparison(op: String, x: &rug::Integer, y: &rug::Integer) -> Rc<Value> {
     /* Compares two integer values */
 
     let result = match &op[..] {
@@ -177,9 +180,9 @@ pub (in crate::eval) fn comparison(op: String, parameters: Rc<Value>, env: &Env)
     let res1 = resolve_value(a, env);
     let res2 = resolve_value(b, env);
 
-    let result = match (&*res1, &*res2) {
+    let result = match (&**res1, &**res2) {
         (Value::Integer(x), Value::Integer(y)) => {
-            integer_comparison(op, x.clone(), y.clone())
+            integer_comparison(op, x, y)
         },
 
         (Value::Integer(x), Value::Float(y)) => {
@@ -223,13 +226,13 @@ pub (in crate::eval) fn boolean(op: String, parameters: Rc<Value>, env: &Env) ->
 
     let mut plist_iter = parameter_list.iter();
     let k = plist_iter.next().unwrap();
-    let x = if let Value::Bool(b) = *resolve_value(plist_iter.next().unwrap(), env) {
+    let x = if let Value::Bool(b) = **resolve_value(plist_iter.next().unwrap(), env) {
         b
     } else {
         panic!("Function '{}' expected boolean arguments", remove_amp!(op));
     };
 
-    let y = if let Value::Bool(b) = *resolve_value(plist_iter.next().unwrap_or(&Value::Bool(true).refcounted()), env) {
+    let y = if let Value::Bool(b) = **resolve_value(plist_iter.next().unwrap_or(&Value::Bool(true).refcounted()), env) {
         b
     } else {
         panic!("Function '{}' expected boolean arguments", remove_amp!(op));
