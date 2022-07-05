a986decb2b1073c71d259f241e9aa341c68d10b5
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-07-05 17:57:33 +0100

    format_names() now takes a &Rc<>

--- src/preproc/fmt.rs
+++ src/preproc/fmt.rs
@@ -2,21 +2,21 @@ use crate::read::Value;
 use std::rc::Rc;
 
 
-pub (in crate::preproc) fn format_names(value: Rc<Value>) -> Rc<Value> {
+pub (in crate::preproc) fn format_names(value: &Rc<Value>) -> Rc<Value> {
     /* Appends an ampersand to each name to avoid collision with 'no-continuation' */
 
-    return match &*value {
+    return match &**value {
         Value::Name(name) => {
             Value::Name(format!("&{}", name)).rc()
         },
 
         Value::Cons { car, cdr } => {
             Rc::new(Value::Cons {
-                car: format_names(Rc::clone(car)),
-                cdr: format_names(Rc::clone(cdr))
+                car: format_names(car),
+                cdr: format_names(cdr)
             })
         },
 
-        _ => value
+        _ => Rc::clone(value)
     }
 }
--- src/preproc/preprocessor.rs
+++ src/preproc/preprocessor.rs
@@ -6,7 +6,7 @@ use std::rc::Rc;
 pub fn preprocess(expr: Rc<Value>) -> Rc<Value> {
     /* Preprocesses a value */
 
-    let formatted = fmt::format_names(expr);
+    let formatted = fmt::format_names(&expr);
     let if_rearranged = cps::move_ifs(formatted);
     let cps_converted = cps::convert(if_rearranged, None);
 
