9d805f194b1f589d87cb3276d47de9387dc0cc31
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-07-16 14:20:38 +0100

    Created a rough repl

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,42 @@ mod read;
 mod eval;
 mod preprocess;
 
+use std::io::Write;
+
 fn main() {
     /* Interface */
 
-    let filename: String = std::env::args()
-                            .filter(| s| s.ends_with(".lzp"))
-                            .next()
-                            .expect("liszp: no filename provided");
+    let mut filename = None;
+
+    for arg in std::env::args() {
+        if arg.ends_with(".lzp") {
+            filename = Some(arg);
+        }
+    }
 
     let mut evaluator = eval::Evaluator::new();
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
 
-    evaluator.eval_file(filename);
+    match filename {
+        Some(fname) => evaluator.eval_file(fname),
+
+        None => loop {
+            let mut input_string = String::new();
+
+            print!("> ");
+            stdout.flush().expect("some error message");
+
+            stdin.read_line(&mut input_string).expect("Liszp: failed to read line from stdin");
+
+            let expr = match read::read(&input_string, "<repl>".into()).as_slice() {
+                [e] => e.clone(),
+                _ => panic!("Liszp: repl can only read one expr at a time")
+            };
+
+            let preprocessed = preprocess::preprocess(expr);
+
+            println!("{}", evaluator.eval(&preprocessed));
+        }
+    }
 }
