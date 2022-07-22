245030f2c2e2212ec5d575f59c44eba2e9ec3834
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-07-22 17:59:15 +0100

    Created a preprocess() function

--- src/preprocess/preprocessor.rs
+++ src/preprocess/preprocessor.rs
@@ -30,14 +30,27 @@ impl Preprocessor {
         let mut preprocessed = vec![];
 
         for expr in exprs.iter() {
-            if let Some(macro_expanded) = self.macro_expander.expand_macros(expr)? {
-                let formatted = fmt::format_names(&macro_expanded);
-                let cps_converted = cps::convert_expr(&formatted)?;
-
-                preprocessed.push(cps_converted);
+            if let Some(v) = self.preprocess(expr)? {
+                preprocessed.push(v);
             }
         }
 
         Ok(preprocessed)
+    }
+
+
+    pub fn preprocess(&mut self, expr: &Rc<Value>) -> Result<Option<Rc<Value>>, Error> {
+        /* Preprocesses an expression */
+
+        if let Some(macro_expanded) = self.macro_expander.expand_macros(expr)? {
+            let formatted = fmt::format_names(&macro_expanded);
+            let cps_converted = cps::convert_expr(&formatted)?;
+
+            self.preprocessed.push(cps_converted.clone());
+
+            Ok(Some(cps_converted))
+        } else {
+            Ok(None)
+        }
     }
 }
