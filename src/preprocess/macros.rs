4f3ba5e388d3186328edf693540def44417c8675
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-07-23 18:45:52 +0100

    Everything is broken

--- src/preprocess/macros.rs
+++ src/preprocess/macros.rs
@@ -21,7 +21,7 @@ use std::{
 /* Macro struct */
 
 #[allow(dead_code)]
-struct Macro {
+pub struct Macro {
     name: Rc<Value>,
     args: Rc<Value>,
     body: Rc<Value>
@@ -29,115 +29,7 @@ struct Macro {
 
 
 impl Macro {
-    #[allow(dead_code)]
-    fn to_executable_expression(&self, supplied_args: &[Rc<Value>]) -> Rc<Value> {
-        /* Creates an executable expression from self and supplied arguments */
-
-        let macro_as_function = refcount_list![
-            Value::Name("&lambda".into()).rc(),
-            self.args.clone(),
-            self.body.clone()
-        ];
-
-        let mut quoted_args = Vec::with_capacity(supplied_args.len());
-
-        for arg in supplied_args.iter() {
-            quoted_args.push(Value::Quote(arg.clone()).rc());
-        }
-
-        Value::Cons {
-            car: macro_as_function,
-            cdr: Value::cons_list(&quoted_args)
-        }.rc()
-    }
-}
-
-
-/* Macro expander */
-
-#[allow(dead_code)]
-pub struct MacroExpander {
-    macros: HashMap<String, Macro>,
-}
-
-
-impl MacroExpander {
-    #[allow(dead_code)]
-    pub fn new() -> Self {
-        /* Creates a new MacroExpander */
-
-        MacroExpander {
-            macros: HashMap::new()
-        }
-    }
-
-
-    #[allow(dead_code)]
-    pub fn expand_macros(&mut self, expr: &Rc<Value>, evaluator: &mut Evaluator) -> Result<Option<Rc<Value>>, Error> {
-       /* Expands all macros in an expression
-        *
-        * Returns
-        * -------
-        * - Err(..)      : an error, if one occurs
-        * - Ok(None)     : if expr is a macro defintion
-        * - Ok(Some(..)) : an expression with all macros expanded
-        */
-
-        if let Some(new_macro) = self.parse_macro_definition(expr)? {
-            self.add_macro(new_macro)?;
-            return Ok(None);
-        }
-
-        match expr.to_list() {
-            Some(components) => {
-                if components.is_empty() {
-                   return Ok(Some(expr.clone()));
-                }
-
-                match self.macros.get(&components[0].name()) {
-                    Some(m) => {
-                        let supplied_args = &components[1..];
-                        let executable_expression = m.to_executable_expression(supplied_args);
-
-                        evaluator.eval(&executable_expression)
-                                 .map(|v| Some(v.clone()))
-                    }
-
-                    None => {
-                        let mut new_components = vec![];
-
-                        for comp in components.iter() {
-                            match self.expand_macros(comp, evaluator)? {
-                                Some(v) => new_components.push(v),
-                                None => return new_error!("Cannot define a macro inside an expression").into()
-                            }
-                        }
-
-                        Ok(Some(Value::cons_list(&new_components)))
-                    }
-                }
-            }
-
-            None => Ok(Some(expr.clone()))
-        }
-    }
-
-
-    #[allow(dead_code)]
-    fn add_macro(&mut self, m: Macro) -> Result<(), Error> {
-        /* Adds a macro to the scope */
-
-        let macro_name = m.name.name();
-
-        match self.macros.insert(m.name.name(), m) {
-            Some(_) => new_error!("macro '{}' has already been defined", macro_name).into(),
-            None => Ok(())
-        }
-    }
-
-
-    #[allow(dead_code)]
-    fn parse_macro_definition(&mut self, expr: &Rc<Value>) -> Result<Option<Macro>, Error> {
+    fn from_expr(expr: &Rc<Value>) -> Result<Option<Macro>, Error> {
         /* Attempts to parse a macro definition */
 
         let components = match expr.to_list() {
@@ -183,5 +75,91 @@ impl MacroExpander {
                 body
             }
         ))
+    }
+
+
+    fn to_executable_expression(&self, supplied_args: &[Rc<Value>]) -> Rc<Value> {
+        /* Creates an executable expression from self and supplied arguments */
+
+        let macro_as_function = refcount_list![
+            Value::Name("&lambda".into()).rc(),
+            self.args.clone(),
+            self.body.clone()
+        ];
+
+        let mut quoted_args = Vec::with_capacity(supplied_args.len());
+
+        for arg in supplied_args.iter() {
+            quoted_args.push(Value::Quote(arg.clone()).rc());
+        }
+
+        Value::Cons {
+            car: macro_as_function,
+            cdr: Value::cons_list(&quoted_args)
+        }.rc()
+    }
+}
+
+
+/* Macro expansion */
+
+pub fn expand_macros(expr: &Rc<Value>, evaluator: &mut Evaluator) -> Result<Option<Rc<Value>>, Error> {
+    /* Expands all macros in an expression
+     *
+     * Returns
+     * -------
+     * - Err(..)      : an error, if one occurs
+     * - Ok(None)     : if expr is a macro defintion
+     * - Ok(Some(..)) : an expression with all macros expanded
+     */
+
+     if let Some(new_macro) = Macro::from_expr(expr, )? {
+         add_macro(evaluator, new_macro)?;
+
+         return Ok(None);
+     }
+
+     match expr.to_list() {
+         Some(components) => {
+             if components.is_empty() {
+                return Ok(Some(expr.clone()));
+             }
+
+             match evaluator.macros.get(&components[0].name()) {
+                 Some(m) => {
+                     let supplied_args = &components[1..];
+                     let executable_expression = m.to_executable_expression(supplied_args);
+
+                     evaluator.eval(&executable_expression)
+                              .map(|v| Some(v.clone()))
+                 }
+
+                 None => {
+                     let mut new_components = vec![];
+
+                     for comp in components.iter() {
+                         match expand_macros(comp, evaluator)? {
+                             Some(v) => new_components.push(v),
+                             None => return new_error!("Cannot define a macro inside an expression").into()
+                         }
+                     }
+
+                     Ok(Some(Value::cons_list(&new_components)))
+                 }
+             }
+         }
+
+         None => Ok(Some(expr.clone()))
+     }
+ }
+
+ fn add_macro(evaluator: &mut Evaluator, m: Macro) -> Result<(), Error> {
+    /* Adds a macro to the scope */
+
+    let macro_name = m.name.name();
+
+    match evaluator.macros.insert(m.name.name(), m) {
+        Some(_) => new_error!("macro '{}' has already been defined", macro_name).into(),
+        None => Ok(())
     }
 }
--- src/preprocess/preprocessor.rs
+++ src/preprocess/preprocessor.rs
@@ -7,36 +7,15 @@ use crate::{
 
 use std::rc::Rc;
 
+pub fn preprocess(expr: &Rc<Value>, evaluator: &mut Evaluator) -> Result<Option<Rc<Value>>, Error> {
+    /* Preprocesses an expression */
 
-pub struct Preprocessor {
-    macro_expander: macros::MacroExpander,
-    preprocessed: Vec<Rc<Value>>
-}
+    if let Some(macro_expanded) = macros::expand_macros(expr, evaluator)? {
+        let formatted = fmt::format_names(&macro_expanded);
+        let cps_converted = cps::convert_expr(&formatted)?;
 
-
-impl Preprocessor {
-    pub fn new() -> Self {
-        /* Creates a new Preprocessor */
-
-        Preprocessor {
-            macro_expander: macros::MacroExpander::new(),
-            preprocessed: vec![]
-        }
-    }
-
-
-    pub fn preprocess(&mut self, expr: &Rc<Value>, evaluator: &mut Evaluator) -> Result<Option<Rc<Value>>, Error> {
-        /* Preprocesses an expression */
-
-        if let Some(macro_expanded) = self.macro_expander.expand_macros(expr, evaluator)? {
-            let formatted = fmt::format_names(&macro_expanded);
-            let cps_converted = cps::convert_expr(&formatted)?;
-
-            self.preprocessed.push(cps_converted.clone());
-
-            Ok(Some(cps_converted))
-        } else {
-            Ok(None)
-        }
+        Ok(Some(cps_converted))
+    } else {
+        Ok(None)
     }
 }
